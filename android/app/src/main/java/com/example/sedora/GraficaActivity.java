2704134d802aa7afd43c1265e619f09e84d7e9e6
Ariel <ariel507182003@gmail.com>
2024-10-13 15:44:42 +0200

    Cambio de Pantallas Funcional
    Ya se logra cambiar de pagina de progreso  a la grafica.

--- android/app/src/main/java/com/example/sedora/GraficaActivity.java
+++ android/app/src/main/java/com/example/sedora/GraficaActivity.java
@@ -1,7 +1,11 @@
 package com.example.sedora;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,22 +13,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class GraficaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_grafica);
+
+        ImageView button=findViewById(R.id.boton_Regreso);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(GraficaActivity.this,ProgresoActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-
-            //BORRAR ESTAS LINEAS AL TERMINAR LA TAREA
-            Intent intent = new Intent(MainActivity.this, ProgresoActivity.class);
-            startActivity(intent);
             return insets;
-
         });
+
     }
 }
--- android/app/src/main/java/com/example/sedora/ProgresoActivity.java
+++ android/app/src/main/java/com/example/sedora/ProgresoActivity.java
@@ -2,6 +2,7 @@ package com.example.sedora;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,22 +10,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ProgresoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.pagina_progreso);
+
+        View horasSentado =findViewById(R.id.HorasSentado);
+
+        horasSentado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent  intent=new Intent(ProgresoActivity.this,GraficaActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-
-            //BORRAR ESTAS LINEAS AL TERMINAR LA TAREA
-            Intent intent = new Intent(MainActivity.this, ProgresoActivity.class);
-            startActivity(intent);
             return insets;
-
         });
     }
 }
