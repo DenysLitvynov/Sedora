d38ac2ae4f36e83e4125fe1f74ba4c1de2dff426
Ariel <ariel507182003@gmail.com>
2024-10-13 16:38:16 +0200

    Pagina de Progreso  funcional

--- android/app/src/main/java/com/example/sedora/GraficaActivity.java
+++ android/app/src/main/java/com/example/sedora/GraficaActivity.java
@@ -1,11 +1,13 @@
 package com.example.sedora;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class GraficaActivity extends AppCompatActivity {
 
+   private  TextView titulo_Grafica;
+   private String  grafica_elegida;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +27,16 @@ public class GraficaActivity extends AppCompatActivity {
         setContentView(R.layout.main_grafica);
 
         ImageView button=findViewById(R.id.boton_Regreso);
+
+        //Recuperamos el string establecido en ProgresoActividado al elegir una grafica.
+        grafica_elegida= getIntent().getStringExtra("Titulo_de_Grafica");
+
+        titulo_Grafica=findViewById(R.id.Titulo);
+
+        titulo_Grafica.setText(grafica_elegida);
+
+        cargarDatos();
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +52,42 @@ public class GraficaActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
+
+    //Esta dependiendo que se elegio, carga los datos(POR AHORA SON FALSOS)
+
+    @SuppressLint("SetTextI18n")
+    private void cargarDatos(){
+
+        TextView vmin= findViewById(R.id.Valor_min);
+        TextView vprom= findViewById(R.id.Valor_promedio);
+        TextView vmax= findViewById(R.id.Valor_max);
+
+        switch (grafica_elegida){
+            case "Horas Sentado":
+                vmin.setText("5hrs");
+                vprom.setText("12hrs");
+                vmax.setText("19hrs");
+                break;
+
+            case "Horas Sensibles":
+                vmin.setText("5:00");
+                vprom.setText("15:00");
+                vmax.setText("21:00");
+                break;
+
+            case "Progreso Avisos":
+                vmin.setText("5");
+                vprom.setText("10");
+                vmax.setText("20");
+                break;
+
+            case "Avisos Ignorados":
+                vmin.setText("1");
+                vprom.setText("7");
+                vmax.setText("22");
+                break;
+        }
+    }
+
 }
--- android/app/src/main/java/com/example/sedora/ProgresoActivity.java
+++ android/app/src/main/java/com/example/sedora/ProgresoActivity.java
@@ -12,7 +12,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ProgresoActivity extends AppCompatActivity {
 
+
+
     @Override
+    //================================INICIO DE ON CREATE==============================================
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -20,19 +23,66 @@ public class ProgresoActivity extends AppCompatActivity {
 
         View horasSentado =findViewById(R.id.HorasSentado);
 
+        View horasSensibles =findViewById(R.id.HorasSensibles);
+
+        View progresoAvisos =findViewById(R.id.ProgresoAvisos);
+
+        View avisosIgnorados =findViewById(R.id.AvisosIgnorados);
+
+        //________________________________________________________________________________________
+
+        //ON CLICK LISTENERS PARA LOS BOTONES(INICIO)
+
+        //________________________________________________________________________________________
         horasSentado.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent  intent=new Intent(ProgresoActivity.this,GraficaActivity.class);
-                startActivity(intent);
+                establecer_Titulo_Grafica("Horas Sentado");
+            }
+        });
+
+        horasSensibles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               establecer_Titulo_Grafica("Horas Sensibles");
 
             }
         });
 
+        progresoAvisos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                establecer_Titulo_Grafica("Progreso Avisos");
+
+            }
+        });
+
+        avisosIgnorados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                establecer_Titulo_Grafica("Avisos Ignorados");
+            }
+        });
+        //________________________________________________________________________________________
+
+        //ON CLICK LISTENERS PARA LOS BOTONES(FIN)
+
+        //________________________________________________________________________________________
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    //================================FIN DE ON CREATE==============================================
+
+    //METODO PARA ESTABLECER EL TITULO DE LA GRAFICA / DATOS A CARGAR
+
+    private void establecer_Titulo_Grafica(String paginaSelecionada){
+        Intent intent = new Intent(ProgresoActivity.this, GraficaActivity.class);
+        intent.putExtra("Titulo_de_Grafica",paginaSelecionada);
+        startActivity(intent);
+
     }
 }
